6dbc8d9fad69b3e7a138264ced010826988b8b85
RASIT <shjh0815@naver.com>
2026-09-17 16:14:11 +0900

    0013

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -1,10 +1,13 @@
 package com.back.boundedContext.post.app;
 
+import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
 import com.back.global.rsData.RsData;
+import com.back.shared.post.dto.PostDto;
+import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,20 @@ import org.springframework.stereotype.Service;
 public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
+    private final MemberFacade memberFacade;
 
     public RsData<Post> write(Member author, String title, String content){
         Post post = postRepository.save(new Post(author, title, content));
 
-        return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
+        eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
+
+        String randomSecureTip = memberFacade.getRandomSecureTip();
+
+        return new RsData<>(
+                "201-1",
+                "%d번 글이 생성되었습니다. 보안 팁 : %s"
+                        .formatted(post.getId(), randomSecureTip),
+                post
+        );
     }
 }
