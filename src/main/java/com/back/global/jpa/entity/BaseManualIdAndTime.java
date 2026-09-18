b67a5ddb8070012249e34e12ab5deca167567d2e
RASIT <shjh0815@naver.com>
2026-09-18 16:27:59 +0900

    0023

---
+++ src/main/java/com/back/global/jpa/entity/BaseManualIdAndTime.java
@@ -0,0 +1,31 @@
+package com.back.global.jpa.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@Getter
+public class BaseManualIdAndTime {
+    @Id
+    private int id;
+
+    @CreatedDate
+    private LocalTime createDate;
+
+    @LastModifiedDate
+    private LocalTime modifyDate;
+
+    public BaseManualIdAndTime(int id){
+        this.id = id;
+    }
+}
