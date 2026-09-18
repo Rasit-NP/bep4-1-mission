1956fabc3916a705272a3d0c61d15de966052919
RASIT <shjh0815@naver.com>
2026-09-18 10:48:38 +0900

    0021

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -26,13 +26,17 @@ public class PostFacade {
     }
 
     @Transactional
-    public RsData<Post> write(Member author, String title, String content) {
+    public RsData<Post> write(PostMember author, String title, String content) {
         return postWriteUseCase.write(author, title, content);
     }
 
     @Transactional(readOnly = true)
     public Optional<Post> findById(int id) {
         return postRepository.findById(id);
+    }
+
+    public Optional<PostMember> findMemberByUsername(String username){
+        return postMemberRepository.findByUsername(username);
     }
 
     @Transactional
