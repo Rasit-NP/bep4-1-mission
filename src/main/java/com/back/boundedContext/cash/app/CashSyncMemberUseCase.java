e987777ad98001f3a22267660845ce6e9fe6fff4
RASIT <shjh0815@naver.com>
2026-09-21 09:46:57 +0900

    0027

--- src/main/java/com/back/boundedContext/cash/app/CashSyncMemberUseCase.java
+++ src/main/java/com/back/boundedContext/cash/app/CashSyncMemberUseCase.java
@@ -2,6 +2,10 @@ package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.out.CashMemberRepository;
+import com.back.boundedContext.member.domain.Member;
+import com.back.global.eventPublisher.EventPublisher;
+import com.back.shared.cash.dto.CashMemberDto;
+import com.back.shared.cash.event.CashMemberCreatedEvent;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,9 +15,12 @@ import org.springframework.stereotype.Service;
 public class CashSyncMemberUseCase {
 
     private final CashMemberRepository cashMemberRepository;
+    private final EventPublisher eventPublisher;
 
     public CashMember syncMember(MemberDto member){
-        CashMember cashMember = new CashMember(
+        boolean isNew = !cashMemberRepository.existsById(member.getId());
+
+        CashMember _member = new CashMember(
                 member.getId(),
                 member.getCreateDate(),
                 member.getModifyDate(),
@@ -23,6 +30,14 @@ public class CashSyncMemberUseCase {
                 member.getActivityScore()
         );
 
-        return cashMemberRepository.save(cashMember);
+        if (isNew){
+            eventPublisher.publish(
+                new CashMemberCreatedEvent(
+                        new CashMemberDto(_member)
+                )
+            );
+        }
+
+        return _member;
     }
 }
