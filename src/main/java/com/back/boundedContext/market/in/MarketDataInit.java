d541cd37593d83696d9817fbcf0243035adc63fe
RASIT <shjh0815@naver.com>
2026-09-21 11:38:10 +0900

    0031

---
+++ src/main/java/com/back/boundedContext/market/in/MarketDataInit.java
@@ -0,0 +1,36 @@
+package com.back.boundedContext.market.in;
+
+import com.back.boundedContext.market.app.MarketFacade;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@Slf4j
+public class MarketDataInit {
+
+    private final MarketDataInit self;
+    private final MarketFacade marketFacade;
+    private final PostApiClient postApiClient;
+
+    public MarketDataInit(
+            @Lazy MarketDataInit self,
+            MarketFacade marketFacade,
+            PostApiClient postApiClient
+            ){
+        this.self = self;
+        this.marketFacade = marketFacade;
+        this.postApiClient = postApiClient;
+    }
+
+    @Bean
+    @Order(3)
+    public ApplicationRunner marketDataInitApplicationRunner(){
+        return args -> {
+            self.makeBaseProducts();
+        };
+    }
+}
