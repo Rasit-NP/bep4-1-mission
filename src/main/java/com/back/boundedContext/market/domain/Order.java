67a4300c2af7db63c79fca731fc9383162029ebf
RASIT <shjh0815@naver.com>
2026-09-21 16:31:51 +0900

    0035

--- src/main/java/com/back/boundedContext/market/domain/Order.java
+++ src/main/java/com/back/boundedContext/market/domain/Order.java
@@ -1,6 +1,8 @@
 package com.back.boundedContext.market.domain;
 
 import com.back.global.jpa.entity.BaseIdAndTime;
+import com.back.shared.market.dto.OrderDto;
+import com.back.shared.market.event.MarketOrderPaymentRequestedEvent;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -8,6 +10,7 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +26,9 @@ public class Order extends BaseIdAndTime {
 
     @ManyToOne(fetch = LAZY)
     private MarketMember buyer;
+
+    private LocalDateTime requestPaymentDate;
+    private LocalDateTime paymentDate;
     private long price;
     private long salePrice;
 
@@ -50,5 +56,28 @@ public class Order extends BaseIdAndTime {
 
         price += product.getPrice();
         salePrice += product.getSalePrice();
+    }
+
+    public void completePayment(){
+        paymentDate = LocalDateTime.now();
+    }
+
+    public boolean isPaid(){
+        return paymentDate != null;
+    }
+
+    public void requestPayment(Long pgPaymentAmount){
+        requestPaymentDate = LocalDateTime.now();
+
+        publishEvent(
+                new MarketOrderPaymentRequestedEvent(
+                        new OrderDto(this),
+                        pgPaymentAmount
+                )
+        );
+    }
+
+    public void cancelRequestPayment(){
+        requestPaymentDate = null;
     }
 }
--- src/main/java/com/back/boundedContext/market/in/MarketDataInit.java
+++ src/main/java/com/back/boundedContext/market/in/MarketDataInit.java
@@ -185,4 +185,13 @@ public class MarketDataInit {
         cart1.addItem(product3);
         cart1.addItem(product4);
     }
+
+    @Transactional
+    public void makeBasePaidOrders() {
+        Order order1 = marketFacade.findOrderById(1).get();
+
+        if (order1.isPaid())
+            return;
+        marketFacade.requestPayment(order1, 0);
+    }
 }
