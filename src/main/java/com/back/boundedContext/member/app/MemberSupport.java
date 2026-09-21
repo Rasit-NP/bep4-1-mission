7bd36ebb682bce683bedeccdeb922b50fd6cb85a
RASIT <shjh0815@naver.com>
2026-09-21 10:09:29 +0900

    0029

---
+++ src/main/java/com/back/boundedContext/member/app/MemberSupport.java
@@ -0,0 +1,31 @@
+package com.back.boundedContext.member.app;
+
+import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.out.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberSupport {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public long count(){
+        return memberRepository.count();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Member> findById(int id){
+        return memberRepository.findById(id);
+    }
+}
