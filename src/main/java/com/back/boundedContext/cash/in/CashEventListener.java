a16b3acc975b7baee1e1c9f9fd5386bafce5ff4b
RASIT <shjh0815@naver.com>
2026-09-22 12:16:50 +0900

    0038

--- src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
@@ -41,7 +41,7 @@ public class CashEventListener {
 
     @TransactionalEventListener(phase = AFTER_COMMIT)
     @Transactional(propagation = REQUIRES_NEW)
-    public void handle(MarketOrderPaymentRequestedEvent event){
-        cashFacade.handle(event);
+    public void completeOrderPayment(MarketOrderPaymentRequestedEvent event){
+        cashFacade.completeOrderPayment(event.getOrder(), event.getPgPaymentAmount());
     }
 }
